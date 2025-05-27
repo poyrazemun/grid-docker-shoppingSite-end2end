9498ae9bb2cea86bf20affb241875b92de8bd452
bibakunin <ofatih-poyraz@hotmail.com>
2025-05-27 15:56:14 +0200

    headless option is set for firefox.

--- src/main/java/base/Driver.java
+++ src/main/java/base/Driver.java
@@ -1,10 +1,13 @@
 package base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import utils.ConfigReader;
 
 import java.time.Duration;
@@ -12,6 +15,7 @@ import java.time.Duration;
 public class Driver {
 
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private static final Logger logger = LogManager.getLogger(Driver.class);
 
 
     private Driver() {
@@ -21,19 +25,26 @@ public class Driver {
         if (driver.get() == null) {
 
             String browser = System.getProperty("browser") != null ? System.getProperty("browser") : ConfigReader.get("browser").toLowerCase();
-            boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+            logger.info("Testing in browser: {}", browser);
+            boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "true"));
             //Bu yukarıdaki satır şu komut için uygundur: mvn test -Dbrowser=chrome -Dheadless=true
             switch (browser) {
                 case "edge":
                     driver.set(new EdgeDriver());
                     break;
                 case "firefox":
-                    driver.set(new FirefoxDriver());
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    if (isHeadless) {
+                        firefoxOptions.addArguments("-headless");
+                        firefoxOptions.addArguments("--width=1920");
+                        firefoxOptions.addArguments("--height=1080");
+                    }
+                    driver.set(new FirefoxDriver(firefoxOptions));
                     break;
                 case "chrome":
                     ChromeOptions chromeOptions = new ChromeOptions();
                     if (isHeadless) {
-                        chromeOptions.addArguments("--headless");
+                        chromeOptions.addArguments("--headless=new");
                         chromeOptions.addArguments("--window-size=1920,1080");
                     }
                     driver.set(new ChromeDriver(chromeOptions));

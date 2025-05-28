1f863625ec71222cbe1306104a1802800845e50c
bibakunin <ofatih-poyraz@hotmail.com>
2025-05-29 00:07:38 +0200

    Driver is adjusted for grid and docker.

--- src/main/java/base/Driver.java
+++ src/main/java/base/Driver.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import utils.ConfigReader;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.time.Duration;
 
 public class Driver {
@@ -24,43 +26,77 @@ public class Driver {
     public static WebDriver getDriver() {
         if (driver.get() == null) {
 
-            String browser = System.getProperty("browser") != null ? System.getProperty("browser") : ConfigReader.get("browser").toLowerCase();
-            logger.info("Testing in browser: {}", browser);
+            String browser = System.getProperty("browser") != null
+                    ? System.getProperty("browser")
+                    : ConfigReader.get("browser").toLowerCase();
+
+            boolean isRemote = Boolean.parseBoolean(System.getProperty("remote", "false")); // default local
             boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "true"));
-            //Bu yukarıdaki satır şu komut için uygundur: mvn test -Dbrowser=chrome -Dheadless=true
-            switch (browser) {
-                case "edge":
-                    driver.set(new EdgeDriver());
-                    break;
-                case "firefox":
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    if (isHeadless) {
-                        firefoxOptions.addArguments("-headless");
-                        firefoxOptions.addArguments("--width=1920");
-                        firefoxOptions.addArguments("--height=1080");
+
+            logger.info("Browser: {}, Remote: {}, Headless: {}", browser, isRemote, isHeadless);
+
+            try {
+                if (isRemote) {
+                    switch (browser) {
+                        case "firefox":
+                            FirefoxOptions firefoxOptions = new FirefoxOptions();
+                            if (isHeadless) {
+                                firefoxOptions.addArguments("-headless");
+                                firefoxOptions.addArguments("--width=1920");
+                                firefoxOptions.addArguments("--height=1080");
+                            }
+                            driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions));
+                            break;
+
+                        case "chrome":
+                            ChromeOptions chromeOptions = new ChromeOptions();
+                            if (isHeadless) {
+                                chromeOptions.addArguments("--headless=new");
+                                chromeOptions.addArguments("--window-size=1920,1080");
+                            }
+                            driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions));
+                            break;
+
+                        default:
+                            throw new IllegalArgumentException("Remote WebDriver does not support browser: " + browser);
                     }
-                    driver.set(new FirefoxDriver(firefoxOptions));
-                    break;
-                case "chrome":
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    if (isHeadless) {
-                        chromeOptions.addArguments("--headless=new");
-                        chromeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    switch (browser) {
+                        case "firefox":
+                            FirefoxOptions firefoxOptions = new FirefoxOptions();
+                            if (isHeadless) {
+                                firefoxOptions.addArguments("-headless");
+                                firefoxOptions.addArguments("--width=1920");
+                                firefoxOptions.addArguments("--height=1080");
+                            }
+                            driver.set(new FirefoxDriver(firefoxOptions));
+                            break;
+
+                        case "chrome":
+                            ChromeOptions chromeOptions = new ChromeOptions();
+                            if (isHeadless) {
+                                chromeOptions.addArguments("--headless=new");
+                                chromeOptions.addArguments("--window-size=1920,1080");
+                            }
+                            driver.set(new ChromeDriver(chromeOptions));
+                            break;
+
+                        case "edge":
+                            driver.set(new EdgeDriver());
+                            break;
+
+                        default:
+                            throw new IllegalArgumentException("Unsupported local browser: " + browser);
                     }
-                    driver.set(new ChromeDriver(chromeOptions));
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unsupported browser: " + browser);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("WebDriver could not be initialized.");
             }
-
-
-            driver.get().manage().timeouts().implicitlyWait(
-                    Duration.ofSeconds(Integer.parseInt(ConfigReader.get("implicit_wait")))
-            );
-            driver.get().manage().window().maximize();
         }
         return driver.get();
     }
+
 
     public static void quitDriver() {
         if (driver.get() != null) {

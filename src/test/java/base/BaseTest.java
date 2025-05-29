4fa664ef2db566749b04413045e10f0ba614f366
bibakunin <ofatih-poyraz@hotmail.com>
2025-05-29 08:55:32 +0200

    setEnvironment and copyCategoriesJson methods are added to BeforeSuite to have better reporting.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,22 +5,58 @@ import listeners.RetryTransformer;
 import listeners.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.LandingPage;
 import pages.ProductsPage;
 import utils.ConfigReader;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
+import java.util.Properties;
 
 @Listeners({TestListener.class, LoggingListener.class, RetryTransformer.class})
 public class BaseTest {
     protected WebDriver driver;
     public static LandingPage landingPage;
     protected ProductsPage productsPage;
+
+    @BeforeSuite
+    public void setEnvironment() {
+        try {
+            Properties props = new Properties();
+            props.setProperty("Browser", ConfigReader.get("browser"));
+            props.setProperty("Environment", "QA");
+            props.setProperty("Platform", System.getProperty("os.name"));
+
+            File file = new File("target/allure-results/environment.properties");
+            file.getParentFile().mkdirs();
+            FileOutputStream fos = new FileOutputStream(file);
+            props.store(fos, "Environment Details");
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeSuite
+    public void copyCategoriesJson() {
+        try {
+            Path src = Paths.get("src/test/resources/categories.json");
+            Path dest = Paths.get("target/allure-results/categories.json");
+            Files.createDirectories(dest.getParent());
+            Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @BeforeMethod(alwaysRun = true)
     public void setUp(Method method) {
